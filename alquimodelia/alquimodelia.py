8ae977bd955d8757b659ef36af151362f87c6266
Joao <joao@tesselo.com>
2022-08-18 12:18:56 +0100

    feat(arch): change to classes

--- alquimodelia/alquimodelia.py
+++ alquimodelia/alquimodelia.py
@@ -1,7 +1,5 @@
-import keras_unet
-
-from alquimodelia.pixel import Pixel_model
-from alquimodelia.unet_arch import UNET
+import tensorflow as tf
+from tensorflow.keras.layers import Input
 
 # for classes sigmoid sofmax stupid
 
@@ -12,46 +10,41 @@ from alquimodelia.unet_arch import UNET
 # https://github.com/maxvfischer/keras-image-segmentation-loss-functions
 
 
-def ModelMagia(
-    model_type,
-    timesteps,
-    width,
-    height,
-    padding,
-    num_bands,
-    num_classes,
-    activation_final=None,
-):
-    if model_type == "pixel":
-        model = Pixel_model(
-            timesteps,
-            num_bands,
-            num_classes,
-            activation_final=None,
-            return_last_layer=False,
-            classifyer=True,
+class ModelMagia(tf.keras.Model):
+    def define_input_shape(self):
+        raise NotImplementedError
+
+    def get_input_layer(self):
+        self.define_input_shape()
+        self.input_layer = Input(self.model_input_shape)
+
+    def get_output_layer(self):
+        raise NotImplementedError
+
+    def get_last_layer_activation(self):
+        raise NotImplementedError
+
+    def __init__(
+        self,
+        timesteps=None,
+        width=None,
+        height=None,
+        padding=None,
+        num_bands=None,
+        num_classes=None,
+        activation_final=None,
+    ):
+
+        self.num_classes = num_classes
+        self.timesteps = timesteps
+        self.width = width
+        self.height = height
+        self.padding = padding
+        self.num_bands = num_bands
+        self.activation_final = activation_final
+        self.get_input_layer()
+
+        super().__init__(
+            inputs=self.input_layer,
+            outputs=self.get_output_layer(),
         )
-    elif "UNET" in model_type:
-        dimension = model_type.split("_")[0]
-        model = UNET(
-            timesteps,
-            width,
-            height,
-            padding,
-            num_bands,
-            num_classes,
-            dimension=dimension,
-        )
-    elif "keras_unet" in model_type:
-        # Use keras_unet.name_of_model
-        model_name = model_type.replace("keras_unet.", "")
-        model = getattr(keras_unet.models, model_name)
-        model = model(
-            input_shape=(width, height, num_bands),
-            use_batch_norm=True,
-            num_classes=num_classes,
-            filters=64,
-            dropout=0.2,
-            output_activation=activation_final,
-        )
-    return model
