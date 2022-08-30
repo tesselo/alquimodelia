753be4f7408791c6a67c257be2fcccfe5c7e633b
Joao <joao@tesselo.com>
2022-08-30 15:48:26 +0100

    refactor(typing): refactor of functions

--- alquimodelia/rnn_lstm_arch.py
+++ alquimodelia/rnn_lstm_arch.py
@@ -1,7 +1,7 @@
+from functools import cached_property
 from typing import Tuple
 
 from tensorflow.keras.layers import (
-    GRU,
     LSTM,
     BatchNormalization,
     Bidirectional,
@@ -20,26 +20,33 @@ class RnnLSTM(ModelMagia):
         self,
         lstm_units: Tuple[int, int] = (120, 80),
         **kwargs,
-    ):  
-        self.model_input_shape = None
+    ):
         self.lstm_units = lstm_units
         super().__init__(**kwargs)
 
-    def define_input_shape(self):
+    @cached_property
+    def model_input_shape(self):
         if self.data_format == "channels_first":
-            self.model_input_shape = (
+            return (
                 self.num_bands,
                 self.timesteps,
             )
         elif self.data_format == "channels_last":
-            self.model_input_shape = (self.timesteps, self.num_bands)
+            return (self.timesteps, self.num_bands)
 
-    def get_output_layer(self):
+    def output_layer(self):
         x = BatchNormalization()(self.input_layer)
-        x = LSTM(self.lstm_units[0], return_sequences=True, dropout=0.2, recurrent_dropout=0.2)(x)
+        x = LSTM(
+            self.lstm_units[0],
+            return_sequences=True,
+            dropout=0.2,
+            recurrent_dropout=0.2,
+        )(x)
         x = MaxPooling1D(pool_size=2)(x)
         x = BatchNormalization()(x)
-        x = Bidirectional(LSTM(self.lstm_units[1], dropout=0.2, recurrent_dropout=0.2))(x)
+        x = Bidirectional(LSTM(self.lstm_units[1], dropout=0.2, recurrent_dropout=0.2))(
+            x
+        )
         x = BatchNormalization()(x)
         x = Dropout(0.3)(x)
         x = Dense(120, activation="relu")(x)
--- alquimodelia/unet_arch.py
+++ alquimodelia/unet_arch.py
@@ -1,3 +1,6 @@
+from functools import cached_property
+from typing import Any, Dict, List, Tuple
+
 import tensorflow as tf
 from tensorflow.keras.layers import (
     BatchNormalization,
@@ -15,31 +18,10 @@ from tensorflow.keras.layers import (
 
 from alquimodelia.alquimodelia import ModelMagia
 
-# momentum must be big so the different batches dont do much damage
-# last conv must have a stride and kernel of 1, with a density of num classes
-
-# For classification sigmoid or softmax in last layer
-# RELU in the middle
-# linear in last for linear
-
-# Number of steps must be a log2(size) || actually max number should be when the small filter has 8 px so the 3 kernel still makes sense
-
-# Try dinamic filters
-# They change at each step
-# They stay the same all way
-# different for contract and for expandig
-#
-
-# Try attention gate
-#
-
-# dafuq is this UpSampling2D?
-
-
 # based on https://towardsdatascience.com/understanding-semantic-segmentation-with-unet-6be4f42d4b47
 
 
-def count_number_divisions(size, count, by=2, limit=2):
+def count_number_divisions(size: int, count: int, by: int = 2, limit: int = 2):
     """
     Count the number of possible steps.
 
@@ -83,7 +65,7 @@ class UNet(ModelMagia):
         kernel_initializer: str = "he_normal",
         **kwargs,
     ):
-        self.number_of_conv_layers = number_of_conv_layers
+        self._number_of_conv_layers = number_of_conv_layers
         self.n_filters = n_filters
         self.kernel_size = kernel_size
         self.batchnorm = batchnorm
@@ -94,20 +76,20 @@ class UNet(ModelMagia):
         self.kernel_initializer = kernel_initializer
         super().__init__(**kwargs)
 
-    def get_number_convolution_layers(self):
-        # Set the number of steps.
-        # This will build the max number of steps but sometimes the max is not the best.
-        number_of_layers = []
-        if self.data_format == "channels_first":
-            study_shape = self.model_input_shape[1:]
-        elif self.data_format == "channels_last":
-            study_shape = self.model_input_shape[:-1]
-        for size in study_shape:
-            number_of_layers.append(count_number_divisions(size, 0))
-        if self.number_of_conv_layers == 0:
-            self.number_of_conv_layers = min(number_of_layers)
+    @cached_property
+    def number_of_conv_layers(self):
+        if self._number_of_conv_layers != 0:
+            number_of_layers = []
+            if self.data_format == "channels_first":
+                study_shape = self.model_input_shape[1:]
+            elif self.data_format == "channels_last":
+                study_shape = self.model_input_shape[:-1]
+            for size in study_shape:
+                number_of_layers.append(count_number_divisions(size, 0))
 
-        return
+            self._number_of_conv_layers = min(number_of_layers)
+
+        return self._number_of_conv_layers
 
     def inside_upsampling(self, inputs, channels, upscale_factor):
 
@@ -140,15 +122,15 @@ class UNet(ModelMagia):
 
     def convolution_block(
         self,
-        input_tensor,
-        n_filters,
-        kernel_size=3,
-        batchnorm=True,
-        data_format="channels_first",
-        padding="same",
-        activation="relu",
-        kernel_initializer="he_normal",
-    ):
+        input_tensor: tf.Layer,
+        n_filters: int,
+        kernel_size: int = 3,
+        batchnorm: bool = True,
+        data_format: str = "channels_first",
+        padding: str = "same",
+        activation: str = "relu",
+        kernel_initializer: str = "he_normal",
+    ) -> tf.Layer:
         # first layer
         x = self.Conv(
             filters=n_filters,
@@ -175,16 +157,16 @@ class UNet(ModelMagia):
 
     def contracting_block(
         self,
-        input_img,
-        n_filters=16,
-        batchnorm=True,
-        dropout=0.5,
-        kernel_size=3,
-        strides=2,
-        data_format="channels_last",
-        padding="same",
-        activation="relu",
-    ):
+        input_img: tf.Layer,
+        n_filters: int = 16,
+        batchnorm: bool = True,
+        dropout: float = 0.25,
+        kernel_size: int = 3,
+        strides: int = 2,
+        data_format: str = "channels_last",
+        padding_style: str = "same",
+        activation: str = "relu",
+    ) -> Tuple[tf.Layer, tf.Layer]:
         c1 = self.convolution_block(
             input_img,
             n_filters=n_filters,
@@ -192,30 +174,30 @@ class UNet(ModelMagia):
             batchnorm=batchnorm,
             data_format=data_format,
             activation=activation,
-            padding=padding,
+            padding=padding_style,
         )
-        p1 = self.MaxPooling(strides, padding=padding)(c1)
-        p1 = self.SpatialDropout(dropout * 0.5, data_format=data_format)(p1)
+        p1 = self.MaxPooling(strides, padding=padding_style)(c1)
+        p1 = self.SpatialDropout(dropout, data_format=data_format)(p1)
         return p1, c1
 
     def expansive_block(
         self,
-        ci,
-        cii,
-        n_filters=16,
-        batchnorm=True,
-        dropout=0.5,
-        kernel_size=3,
-        strides=2,
-        data_format="channels_first",
-        activation="relu",
-        padding="same",
-    ):
+        ci: tf.Layer,
+        cii: tf.Layer,
+        n_filters: int = 16,
+        batchnorm: bool = True,
+        dropout: float = 0.5,
+        kernel_size: int = 3,
+        strides: int = 2,
+        data_format: str = "channels_first",
+        activation: str = "relu",
+        padding_style: str = "same",
+    ) -> Tuple[tf.Layer]:
         u = self.ConvTranspose(
             n_filters,
             kernel_size=kernel_size,
             strides=strides,
-            padding=padding,
+            padding=padding_style,
             data_format=data_format,
         )(ci)
         u = concatenate([u, cii])
@@ -227,11 +209,13 @@ class UNet(ModelMagia):
             batchnorm=batchnorm,
             data_format=data_format,
             activation=activation,
-            padding=padding,
+            padding=padding_style,
         )
         return c
 
-    def contracting_loop(self, input_img, contracting_arguments):
+    def contracting_loop(
+        self, input_img: tf.Layer, contracting_arguments: Dict[str, Any]
+    ) -> List[tf.Layer]:
         list_p = [input_img]
         list_c = []
         n_filters = contracting_arguments["n_filters"]
@@ -244,8 +228,10 @@ class UNet(ModelMagia):
             list_c.append(c)
         return list_c
 
-    def expanding_loop(self, list_contracted_layers, expansion_arguments):
-        list_c = [list_contracted_layers[-1]]
+    def expanding_loop(
+        self, contracted_layers: List[tf.Layer], expansion_arguments: Dict[str, Any]
+    ) -> tf.Layer:
+        list_c = [contracted_layers[-1]]
         iterator_expanded_blocks = range(self.number_of_conv_layers)
         iterator_contracted_blocks = reversed(iterator_expanded_blocks)
         n_filters = expansion_arguments["n_filters"]
@@ -253,26 +239,25 @@ class UNet(ModelMagia):
             filter_expansion = 2 ** (c)
             expansion_arguments["n_filters"] = n_filters * filter_expansion
             c4 = self.expansive_block(
-                list_c[i], list_contracted_layers[c], **expansion_arguments
+                list_c[i], contracted_layers[c], **expansion_arguments
             )
             list_c.append(c4)
         return c4
 
-    def get_unet_12t(
+    def deep_neural_network(
         self,
-        n_filters=16,
-        dropout=0.2,
-        batchnorm=True,
-        data_format="channels_last",
-        activation_end="sigmoid",
-        activation_middle="relu",
-        kernel_size=3,
-        padding="same",
-        num_classes=1,
-    ):
+        n_filters: int = 16,
+        dropout: float = 0.2,
+        batchnorm: bool = True,
+        data_format: str = "channels_last",
+        activation_middle: str = "relu",
+        kernel_size: int = 3,
+        padding: str = "same",
+    ) -> tf.Layer:
+        """Build deep neural network."""
 
         input_img = self.input_layer
-        self.get_number_convolution_layers()
+        self.define_number_convolution_layers()
 
         contracting_arguments = {
             "n_filters": n_filters,
@@ -292,8 +277,9 @@ class UNet(ModelMagia):
             "kernel_size": kernel_size,
         }
 
-        list_contracted_layers = self.contracting_loop(input_img, contracting_arguments)
-        unet_output = self.expanding_loop(list_contracted_layers, expansion_arguments)
+        contracted_layers = self.contracting_loop(input_img, contracting_arguments)
+        unet_output = self.expanding_loop(contracted_layers, expansion_arguments)
+
         return unet_output
 
 
@@ -310,14 +296,15 @@ class UNet2D(UNet):
 
         super().__init__(**kwargs)
 
-    def define_input_shape(self):
+    @cached_property
+    def model_input_shape(self):
         if self.data_format == "channels_first":
-            self.model_input_shape = (self.num_bands, self.height, self.width)
+            return (self.num_bands, self.height, self.width)
         elif self.data_format == "channels_last":
-            self.model_input_shape = (self.height, self.width, self.num_bands)
+            return (self.height, self.width, self.num_bands)
 
-    def get_output_layer(self):
-        outputDeep = self.get_unet_12t(
+    def output_layer(self) -> tf.Laye:
+        outputDeep = self.deep_neural_network(
             n_filters=16,
             dropout=0.2,
             batchnorm=True,
@@ -356,25 +343,26 @@ class UNet3D(UNet):
         self.MaxPooling = MaxPooling3D
         super().__init__(**kwargs)
 
-    def define_input_shape(self):
+    @cached_property
+    def model_input_shape(self):
         if self.data_format == "channels_first":
-            self.model_input_shape = (
+            return (
                 self.timesteps,
                 self.num_bands,
                 self.height,
                 self.width,
             )
         elif self.data_format == "channels_last":
-            self.model_input_shape = (
+            return (
                 self.timesteps,
                 self.height,
                 self.width,
                 self.num_bands,
             )
 
-    def get_output_layer(self):
+    def output_layer(self) -> tf.Layer:
 
-        outputDeep = self.get_unet_12t(
+        outputDeep = self.deep_neural_network(
             n_filters=16,
             dropout=0.2,
             batchnorm=True,
